bbb270d193cc30d325d0a5ea09075bd53f1d2fce
b9bengin <morkovwastaken@gmail.com>
2025-05-11 14:37:51 +0200

    Update project branding and navigation styles
    - Changed favicon from vite.svg to ParkDrink.svg and updated the title to "ParkDrink" in index.html.
    - Added ParkDrink.svg file to the public directory.
    - Modified Navigation component to include a new navigationNonExtensionWrapper style for better layout.
    - Updated CSS to use "Passion One" font instead of "Roboto".

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,13 +1,15 @@
-import { makeStyles } from "@fluentui/react-components";
+import { makeStyles, tokens } from "@fluentui/react-components";
 import NavigationProperties from "./NavigationProperties";
+
 
 export default function Navigation(properties: NavigationProperties): React.JSX.Element {
 
-    const { navigationWrapper } = useStyles();
+    const { navigationWrapper, navigationNonExtensionWrapper } = useStyles();
 
     return (
         <div className={navigationWrapper}>
-            -- Navigation --
+            <div className={navigationNonExtensionWrapper}>
+            </div>
 
             <div>
                 {properties.extensionNode}
@@ -20,7 +22,20 @@ const useStyles = makeStyles({
     navigationWrapper: {
         width: "100%",
         height: "3rem",
+        maxHeight: "3rem",
         textAlign: "center",
         backgroundColor: "red",
+        padding: `0 ${tokens.spacingHorizontalXXS}`,
+    },
+    navigationNonExtensionWrapper: {
+        width: "100%",
+        minHeight: "100%",
+        textAlign: "left",
+        backgroundColor: "blue",
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        maxHeight: "100%",
     }
 })
