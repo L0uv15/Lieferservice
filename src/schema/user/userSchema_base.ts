b15e5b57a7b36e01ba0125de735eec9ead4ffb6e
b9bengin <morkovwastaken@gmail.com>
2025-05-18 12:59:16 +0200

    feat: add User type definition and update userSchema_base and userSchema_export files

--- src/schema/user/userSchema_base.ts
+++ src/schema/user/userSchema_base.ts
@@ -3,7 +3,6 @@ import { z } from "zod";
 //TODO: can be reworked once cloud functions are used (e.g. uid -> id)
 const userSchema_base = z.object({
     displayName: z.string(),
-    // metaData: userMetaDataSchema_base,
     email: z.string().email(),
     uid: z.string(),
     isAnonymous: z.boolean(),
--- src/schema/user/userSchema_export.ts
+++ src/schema/user/userSchema_export.ts
@@ -2,6 +2,7 @@ import userMetaDataSchema_export from "./meta/userMetaDataSchema_export";
 import userSchema_api from "./userSchema_api";
 import userSchema_client from "./userSchema_client";
 
+//use UserMutation rather than this whenever possible
 const userSchema_export = userSchema_client.transform<typeof userSchema_api._type>((clientSchema) => {
     const parsed = userMetaDataSchema_export.safeParse(clientSchema.metaData);
     if (parsed.success) {
