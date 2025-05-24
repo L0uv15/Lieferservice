aeabbdc04f87cbcf09b03a018d723544213bc97c
b9bengin <morkovwastaken@gmail.com>
2025-05-25 00:05:59 +0200

    feat: enhance signInWithGooglePopup to handle sign-out on invalid user object

--- src/api/firebase/cloud-functions/functions/signInWithGooglePopup.ts
+++ src/api/firebase/cloud-functions/functions/signInWithGooglePopup.ts
@@ -1,4 +1,4 @@
-import { signInWithPopup } from "firebase/auth";
+import { signInWithPopup, signOut } from "firebase/auth";
 import User from "../../../../schema/user/User";
 import userSchema_import from "../../../../schema/user/userSchema_import";
 import ApiError from "../../../utility/error/ApiError";
@@ -10,7 +10,10 @@ export default async function signInWithGooglePopup(args: SignInWithGooglePopupP
 
     const response = await signInWithPopup(auth, provider);
     const parsedUser = userSchema_import.safeParse(response.user);
-    if (!parsedUser.success) return new ApiError("client", "Invalid user object (96b6e279-1623-432c-b451-926e8859c8e0)", parsedUser.error.message);
+    if (!parsedUser.success) {
+        await signOut(auth);
+        return new ApiError("client", "Invalid user object (96b6e279-1623-432c-b451-926e8859c8e0)", parsedUser.error.message);
+    }
 
     return { ...parsedUser.data };
 }
