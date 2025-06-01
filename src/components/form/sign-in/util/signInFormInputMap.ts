ff2c5e8945e849245a11708d34c61a48a229eecd
b9bengin <morkovwastaken@gmail.com>
2025-06-01 15:45:26 +0200

    --

--- src/components/form/sign-in/util/signInFormInputMap.ts
+++ src/components/form/sign-in/util/signInFormInputMap.ts
@@ -3,5 +3,5 @@ import SignInFormInputName from "./SignInFormInputName";
 
 export const signInFormInputMap = new Map<SignInFormInputName, Required<Pick<InputProperties, "contentBefore" | "placeholder" | "type">> & { name: SignInFormInputName }>([
     ["email", { contentBefore: null, name: "email", placeholder: "Email", type: "email" }],
-    ["password", { contentBefore: null, name: "password", placeholder: "Password", type: "password" }],
+    ["password", { contentBefore: null, name: "password", placeholder: "Passwort", type: "password" }],
 ]);
